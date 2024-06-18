628d05c7e5d172ff845c059555be6228b81e7eca
Julita23 <julibibiloni67@gmail.com>
2024-06-18 20:24:55 -0300

    add fetch structure

--- app/main.js
+++ app/main.js
@@ -1,2 +1,13 @@
 const gridDiv = document.querySelector('.grid');
 
+const urlApiHP = "https://hp-api.onrender.com/api/characters";
+
+fetch(urlApiHP)
+.then((response) => response.json())
+.then((data) => {
+   data.forEach(character => {
+
+   });
+        
+    });
+})
