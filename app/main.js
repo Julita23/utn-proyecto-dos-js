af3734214cdba3d2ca648d67e1313d6a7db2ce00
Julita23 <julibibiloni67@gmail.com>
2024-06-18 20:51:14 -0300

    characters search

--- app/main.js
+++ app/main.js
@@ -1,13 +1,24 @@
 const gridDiv = document.querySelector('.grid');
 
+// API
 const urlApiHP = "https://hp-api.onrender.com/api/characters";
 
+// search for information about each character
 fetch(urlApiHP)
-.then((response) => response.json())
-.then((data) => {
-   data.forEach(character => {
-
-   });
-        
+  .then((response) => response.json())
+  .then((data) => {
+    let charactersHTML = ''; // Definir charactersHTML aquí
+    data.forEach(character => {
+      charactersHTML += `
+        <div class="character">
+          <img src="${character.image}" alt="${character.name}">
+          <h2>${character.name}</h2>
+          <p>${character.house || 'Unknown'}</p>
+        </div>
+      `;
     });
-})
+    gridDiv.innerHTML = charactersHTML;
+  })
+  .catch(error => {
+    console.error('Error fetching the characters:', error);
+  });
